4d1636dbbb320bf32b56dceba2dbd8bc1ad85fca
jaygao1219 <jaygao1219@gmail.com>
2023-05-30 16:07:04 +0800

    [add] 结果分析代码

--- code/result_analysis.py
+++ code/result_analysis.py
@@ -3,6 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from itertools import combinations, product
+import os
+import ast
+import pandas as pd
+from math import sqrt, degrees, atan2, acos, radians, sin, cos
+from sklearn.metrics import mean_squared_error
+
+from radar_config import angle_correct_config
 
 def calculate_distance_azimuth_elevation(point):
     x, y, z = point
@@ -113,3 +120,100 @@ def max_error(result, ground_truth):
     # 计算最大误差
     return np.max([euclidean_distance(p1, p2) for p1, p2 in zip(result, ground_truth)])
 
+def read_config(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith('x\ty\tdistance\truntime'):
+                x, y, distance, _ = lines[lines.index(line)+1].split('\t')
+                return float(x), float(y), float(distance)
+
+def read_result(path):
+    with open(path, 'r') as f:
+        result = ast.literal_eval(f.read())
+    return result
+
+def config_to_coord(x, y, distance):
+    # 将x,y,distance 转换为空间坐标
+    return x*15, y*15, distance
+
+def read_folder(folder_path):
+    config_path = os.path.join(folder_path, 'config.txt')
+    result_path = os.path.join(folder_path, 'result.txt')
+
+    # 读取config.txt并转换坐标
+    x, y, distance = read_config(config_path)
+    coord = config_to_coord(x, y, distance)
+
+    # 读取result.txt
+    result = read_result(result_path)
+
+    return coord, result
+
+def correct_angle(azimuth, elevation):
+    corrected_azimuth = angle_correct_config.azimuth_a * azimuth + angle_correct_config.azimuth_b
+    corrected_elevation = angle_correct_config.elevation_a * elevation + angle_correct_config.elevation_b
+    return corrected_azimuth, corrected_elevation
+
+def spherical_to_cartesian(azimuth, elevation, distance):
+    x = distance * cos(radians(elevation)) * cos(radians(azimuth))
+    y = distance * cos(radians(elevation)) * sin(radians(azimuth))
+    z = distance * sin(radians(elevation))
+    return x, y, z
+
+def calculate_error_for_folder(folder_path):
+    # 读取数据并计算真实坐标
+    config_path = os.path.join(folder_path, 'config.txt')
+    x, y, distance = read_config(config_path)
+    true_coord = config_to_coord(x, y, distance)
+    true_distance, true_azimuth, true_elevation = calculate_distance_azimuth_elevation(true_coord)
+
+    # 读取结果并获取计算的azimuth、elevation和distance
+    result_path = os.path.join(folder_path, 'result.txt')
+    result = read_result(result_path)
+
+    coord_result = []
+    azimuth_result = []
+    elevation_result = []
+    distance_result = []
+
+    for item in result:
+        result_azimuth, result_elevation, result_distance = item[:3]
+        # 先校正
+        result_azimuth, result_elevation = correct_angle(result_azimuth, result_elevation)
+        # 再计算坐标
+        result_coord = calculate_coordinates(result_distance, result_azimuth, result_elevation)
+        azimuth_result.append(result_azimuth)
+        elevation_result.append(result_elevation)
+        distance_result.append(result_distance)
+        coord_result.append(result_coord)
+    
+    coord_rmse = rmse_error(coord_result, [true_coord] * len(coord_result))
+    azimuth_rmse = calculate_rmse(azimuth_result, [true_azimuth] * len(azimuth_result))
+    elevation_rmse = calculate_rmse(elevation_result, [true_elevation] * len(elevation_result))
+    distance_rmse = calculate_rmse(distance_result, [true_distance] * len(distance_result))
+
+    return {
+        'coord_rmse': coord_rmse,
+        'azimuth_rmse': azimuth_rmse,
+        'elevation_rmse': elevation_rmse,
+        'distance_rmse': distance_rmse,
+    }
+
+def calculate_error_for_all():
+    # 创建一个空的 DataFrame 来存储所有的误差数据
+    df = pd.DataFrame(columns=['coord_rmse', 'azimuth_rmse', 'elevation_rmse', 'distance_rmse'])
+
+    for i in range(1, 49):
+        folder_path = f'../data/position/{i}'
+        print(folder_path)
+        error = calculate_error_for_folder(folder_path)
+        df = df.append(error, ignore_index=True)
+
+    return df
+
+# 执行函数并打印误差表
+df = calculate_error_for_all()
+print(df)
+# 保存误差表
+df.to_csv('../data/position_error.csv', index=False)
